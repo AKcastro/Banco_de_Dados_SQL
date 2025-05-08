a7ea32e334e7663f6b86ac10ea908112835ab214
Amanda Castro <akamillycs@gmail.com>
2025-05-08 01:22:25 -0300

    Create 2607.sql

---
+++ BeecrowdSQL/2607.sql
@@ -0,0 +1,27 @@
+Faça uma consulta no Banco de Dados que retorne todas as cidades dos fornecedores, mas em ordem alfabética.
+OBS: Você não deve mostrar cidades repetidas.
+
+providers
+| ID | Name             | Street         | City           | State | 
+|----|------------------|----------------|----------------|-------|
+| 1  | Henrique         | Av Brasil      | Rio de Janeiro | RJ    | 
+| 2  | Marcelo Augusto  | Rua Imigrantes | Belo Horizonte | MG    | 
+| 3  | Caroline Silva   | Av São Paulo   | Salvador       | BA    | 
+| 4  | Guilerme Staff   | Rua Central    | Porto Alegre   | RS    | 
+| 5  | Isabela Moraes   | Av Juiz Grande | Curitiba       | PR    | 
+| 6  | Francisco Accerr | Av Paulista    | São Paulo      | SP    | 
+
+Exemplo de Saída
+| City           |  
+|----------------|
+| Belo Horizonte | 
+| Curitiba       | 
+| Porto Alegre   | 
+| Rio de Janeiro | 
+| Salvador       | 
+| São Paulo      | 
+
+Código:
+select distinct city 
+from providers
+order by city asc;
