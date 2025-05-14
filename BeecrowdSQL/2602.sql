f8dfaa31f95d6c464fc6afa3aa4a523be0225c0c
Amanda Castro <akamillycs@gmail.com>
2025-05-14 13:35:47 -0300

    Update 2602.sql

--- BeecrowdSQL/2602.sql
+++ BeecrowdSQL/2602.sql
@@ -1,5 +1,4 @@
-Sua empresa está fazendo um levantamento de quantos clientes estão cadastrados nos estados, porém, faltou levantar os dados do estado do Rio Grande do Sul.
-Então você deve Exibir o nome de todos os clientes cujo estado seja ‘RS’.
+-- Então você deve Exibir o nome de todos os clientes cujo estado seja ‘RS’.
 
 Costumers
 | ID | Name                      | Street                   | City           | State | Credit_limit |
